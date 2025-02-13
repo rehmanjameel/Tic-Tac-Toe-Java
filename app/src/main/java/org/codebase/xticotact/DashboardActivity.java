fccbf44b589c76f63c33417622c6dd008defd52f
rehmanjameel <abdulrehmancs17@gmail.com>
2025-02-13 16:33:12 +0500

    add dashboard screen

---
+++ app/src/main/java/org/codebase/xticotact/DashboardActivity.java
@@ -0,0 +1,26 @@
+package org.codebase.xticotact;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.codebase.xticotact.databinding.ActivityDashboardBinding;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    ActivityDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+    }
+}
